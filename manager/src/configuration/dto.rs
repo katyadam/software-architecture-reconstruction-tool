8d47eb9924b0bd76d61a22f3208171bdc0f3fa6c
Adam Kattan <kattanadam7@gmail.com>
2025-09-25 15:24:39 +0200

    feature: configuration dtos

---
+++ manager/src/configuration/dto.rs
@@ -0,0 +1,24 @@
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+use utoipa::ToSchema;
+use uuid::Uuid;
+
+#[derive(Debug, Serialize, Deserialize, ToSchema)]
+pub struct PostConfiguration {
+    pub new_configuration_data: Value,
+    #[schema(example = "3fa85f64-5717-4562-b3fc-2c963f66afa6")]
+    pub codebase_uuid: Uuid,
+}
+
+#[derive(Debug, Serialize, Deserialize, ToSchema)]
+pub struct UpdateConfiguration {
+    pub new_configuration_data: Value,
+}
+
+#[derive(Debug, Serialize, Deserialize, ToSchema)]
+pub struct ConfigurationResponse {
+    pub configuration_uuid: Uuid,
+    pub codebase_uuid: Uuid,
+    pub configuration_data: Value,
+    pub created_at: String,
+}
--- manager/src/configuration/model.rs
+++ manager/src/configuration/model.rs
@@ -4,6 +4,8 @@ use diesel::prelude::*;
 use serde_json::Value;
 use uuid::Uuid;
 
+use crate::configuration::dto::ConfigurationResponse;
+
 #[derive(Queryable, Insertable, Debug, Selectable)]
 #[diesel(table_name = crate::schema::configurations)]
 pub struct Configuration {
@@ -14,9 +16,21 @@ pub struct Configuration {
 }
 
 #[derive(Debug, Insertable)]
-#[diesel(table_name = crate::schema::codebases)]
+#[diesel(table_name = crate::schema::configurations)]
 pub struct NewConfiguration {
+    pub configuration_uuid: Uuid,
     pub codebase_uuid: Uuid,
-    pub project_uuid: Uuid,
+    pub configuration_data: Value,
     pub created_at: DateTime<Utc>,
+}
+
+impl Configuration {
+    pub fn to_response(&self) -> ConfigurationResponse {
+        ConfigurationResponse {
+            configuration_uuid: self.configuration_uuid,
+            codebase_uuid: self.codebase_uuid,
+            configuration_data: self.configuration_data.clone(),
+            created_at: self.created_at.to_rfc3339(),
+        }
+    }
 }
