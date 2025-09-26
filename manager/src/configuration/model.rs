5a1d8ae81538eda480e05c90edd251fd643f2099
Adam Kattan <kattanadam7@gmail.com>
2025-09-26 20:46:44 +0200

    ref: added ConfigurationData struct due to checking correct JSON in API

--- manager/src/configuration/model.rs
+++ manager/src/configuration/model.rs
@@ -4,7 +4,9 @@ use diesel::prelude::*;
 use serde_json::Value;
 use uuid::Uuid;
 
+use crate::configuration::dto::ConfigurationData;
 use crate::configuration::dto::ConfigurationResponse;
+use crate::errors::model::ModelError;
 
 #[derive(Queryable, Insertable, Debug, Selectable)]
 #[diesel(table_name = crate::schema::configurations)]
@@ -25,12 +27,15 @@ pub struct NewConfiguration {
 }
 
 impl Configuration {
-    pub fn to_response(&self) -> ConfigurationResponse {
-        ConfigurationResponse {
+    pub fn to_response(&self) -> Result<ConfigurationResponse, ModelError> {
+        let configuration_data: ConfigurationData =
+            serde_json::from_value(self.configuration_data.clone()).map_err(ModelError::from)?;
+
+        Ok(ConfigurationResponse {
             configuration_uuid: self.configuration_uuid,
             project_uuid: self.project_uuid,
-            configuration_data: self.configuration_data.clone(),
+            configuration_data,
             created_at: self.created_at.to_rfc3339(),
-        }
+        })
     }
 }
