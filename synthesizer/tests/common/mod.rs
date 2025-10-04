31e5bb34f879b2a6da8b84f9b0948284200480fb
Adam Kattan <kattanadam7@gmail.com>
2025-10-04 10:50:11 +0200

    fix: tests according to latest version

--- synthesizer/tests/common/mod.rs
+++ synthesizer/tests/common/mod.rs
@@ -2,12 +2,25 @@ use std::sync::Arc;
 
 use actix_web::web::{self, Data, ServiceConfig};
 use neo4rs::Graph;
-use synthesizer::contextmap::{self};
+use synthesizer::contextmap::{
+    self, builder::ContextMapBuilderImpl, repository::ContextMapRepositoryImpl,
+    service::ContextMapServiceImpl,
+};
 
 pub fn configure_test_webapp(graph: Arc<Graph>) -> impl Fn(&mut ServiceConfig) {
     move |config: &mut ServiceConfig| {
-        config
-            .service(web::scope("/context-maps").configure(contextmap::configure))
-            .app_data(Data::new(graph.clone()));
+        let cm_service = get_cm_service(graph.clone());
+        config.service(
+            web::scope("/context-maps")
+                .app_data(Data::new(cm_service))
+                .configure(contextmap::configure),
+        );
     }
+}
+
+fn get_cm_service(graph: Arc<Graph>) -> ContextMapServiceImpl {
+    let cm_repository = ContextMapRepositoryImpl::new(graph);
+    let cm_builder = ContextMapBuilderImpl::new();
+
+    ContextMapServiceImpl::new(cm_repository, cm_builder)
 }
