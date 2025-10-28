1665e5517570ad5214211ea4d7507754dc724eca
Adam Kattan <kattanadam7@gmail.com>
2025-10-28 16:32:57 +0100

    feat-tests: added is_constructor field to callable tests

--- python-extractor/tests/python/callables.rs
+++ python-extractor/tests/python/callables.rs
@@ -28,6 +28,7 @@ fn simple_test() {
             }],
             return_type: None,
             is_async: false,
+            is_constructor: false,
             hash: s!("d74ff0ee8da3b9806b18c877dbf29bbde50b5bd8e4dad7a3a725000feb82e8f1"),
         },
         Callable {
@@ -40,6 +41,7 @@ fn simple_test() {
             }],
             return_type: None,
             is_async: false,
+            is_constructor: false,
             hash: s!("b9ee44d39137ad72fb72086d588215dc00a601ffc9c606f705230b76bb43a501"),
         },
     ];
@@ -65,6 +67,7 @@ fn nested_test() {
             }],
             return_type: None,
             is_async: false,
+            is_constructor: false,
             hash: s!("0105467f2befa106a0483ca9846392a422c7ccb70cdaf93f57d6ba942c4a6b06"),
         },
         Callable {
@@ -77,6 +80,7 @@ fn nested_test() {
             }],
             return_type: None,
             is_async: false,
+            is_constructor: false,
             hash: s!("dfae5a6e06f1ca7eab19bf799456712be278ff21ff66c640fcc49e1cc3a8d52a"),
         },
         Callable {
@@ -89,6 +93,7 @@ fn nested_test() {
             }],
             return_type: None,
             is_async: false,
+            is_constructor: false,
             hash: s!("540334dc3979994b09946ffd275c1060a1de79575a84af03c8950cdca9d19bcb"),
         },
         Callable {
@@ -101,6 +106,7 @@ fn nested_test() {
             }],
             return_type: None,
             is_async: false,
+            is_constructor: false,
             hash: s!("2a93b3636ca0ff3b3009419fd1918de34c39985afdd44a06d100fe06f9bbf2fc"),
         },
     ];
@@ -138,6 +144,7 @@ fn classes_test() {
             ],
             return_type: None,
             is_async: false,
+            is_constructor: true,
             hash: s!("112eaad45fe3a03aa8d62a93b7e059d7af8a0c63a105fa292666ea46bbe555f1"),
         },
         Callable {
@@ -150,6 +157,7 @@ fn classes_test() {
             }],
             return_type: Some(s!("float")),
             is_async: false,
+            is_constructor: false,
             hash: s!("9effd4ed97bd589bbe40b7bfc75aa851617dd39b94519b9c2cecbd23c1b0b2f2"),
         },
         Callable {
@@ -162,6 +170,7 @@ fn classes_test() {
             }],
             return_type: Some(s!("bool")),
             is_async: false,
+            is_constructor: false,
             hash: s!("f0c5f05ef4e51d6fcf5966c194309b7ae2f087ec45e8eac2f9492656cd1ff08e"),
         },
         Callable {
@@ -181,6 +190,7 @@ fn classes_test() {
             ],
             return_type: Some(s!("int")),
             is_async: false,
+            is_constructor: false,
             hash: s!("e148c439e5e3717869a9931caccd290594743bff8ff5b314ffb3b89a6b43b005"),
         },
     ];
@@ -218,6 +228,7 @@ fn classes_imports_test() {
             ],
             return_type: None,
             is_async: false,
+            is_constructor: true,
             hash: s!("112eaad45fe3a03aa8d62a93b7e059d7af8a0c63a105fa292666ea46bbe555f1"),
         },
         Callable {
@@ -230,6 +241,7 @@ fn classes_imports_test() {
             }],
             return_type: None,
             is_async: false,
+            is_constructor: false,
             hash: s!("3051f8e5edfaa306f5bce5b837bdb31bff1ee85083d0b9ec883a4426bd038827"),
         },
         Callable {
@@ -242,6 +254,7 @@ fn classes_imports_test() {
             }],
             return_type: None,
             is_async: false,
+            is_constructor: false,
             hash: s!("1f67e43697d4c9cb1d37345ef1ecc13d787c39e4ba41c0c9c7c3ca6553a8aef6"),
         },
         Callable {
@@ -254,6 +267,7 @@ fn classes_imports_test() {
             }],
             return_type: None,
             is_async: false,
+            is_constructor: false,
             hash: s!("e980235a22a2fb8369a5aadd7ff30ac9b0abc177c1e2a79bc9f1274c5c39c708"),
         },
     ];
