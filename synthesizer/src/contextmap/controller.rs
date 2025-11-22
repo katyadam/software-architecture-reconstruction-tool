cafeb1e11514ccf3af3be965f81fad4b6d4d43d0
Adam Kattan <kattanadam7@gmail.com>
2025-11-22 17:20:15 +0100

    ref: context map - using commit hash

--- synthesizer/src/contextmap/controller.rs
+++ synthesizer/src/contextmap/controller.rs
@@ -9,7 +9,7 @@ use crate::{
     errors::api::ApiError,
 };
 use actix_web::{HttpResponse, Responder, delete, get, post, web};
-use uuid::Uuid;
+use models::api::ProcessFilesIdentifier;
 
 #[utoipa::path(
         post,
@@ -31,45 +31,53 @@ pub async fn create_context_map(
 }
 
 #[utoipa::path(
-        get,
-        path = "/context-maps/{codebase_uuid}",
-        params(
-            ("codebase_uuid", Path, description = "Codebase UUID of the context map to get", example = "3fa85f64-5717-4562-b3fc-2c963f66afa6")
-        ),
-        responses(
-            (status = 200, description = "Context Map successfully retrieved", body = ContextMap),
-            (status = 400, description = "Context Map cannot be retrieved", body = GetContextMapErrorReponse),
-        ),
-    )]
-#[get("/{codebase_uuid}")]
+    get,
+    path = "/context-maps/{codebase_uuid}/{commit_hash}",
+    params(
+        ("codebase_uuid", Path, description = "Codebase UUID of the context map to get", example = "3fa85f64-5717-4562-b3fc-2c963f66afa6"),
+        ("commit_hash", Path, description = "Git commit hash of the context map to get", example = "abc123def4567890abc123def4567890abc123de")
+    ),
+    responses(
+        (status = 200, description = "Context Map successfully retrieved", body = ContextMap),
+        (status = 400, description = "Context Map cannot be retrieved", body = GetContextMapErrorReponse),
+    ),
+)]
+#[get("/{codebase_uuid}/{commit_hash}")]
 pub async fn get_context_map(
     service: web::Data<Arc<ContextMapServiceImpl>>,
-    codebase_uuid_path: web::Path<Uuid>,
+    path: web::Path<ProcessFilesIdentifier>,
 ) -> Result<impl Responder, ApiError> {
-    let codebase_uuid = codebase_uuid_path.into_inner();
-    let cm = service.get_single(codebase_uuid).await?;
+    let identifier = path.into_inner();
+
+    let cm = service
+        .get_single(identifier.codebase_uuid, &identifier.commit_hash)
+        .await?;
 
     Ok(HttpResponse::Ok().json(cm))
 }
 
 #[utoipa::path(
-        delete,
-        path = "/context-maps/{codebase_uuid}",
-        params(
-            ("codebase_uuid", Path, description = "Codebase UUID of the context map to delete", example = "3fa85f64-5717-4562-b3fc-2c963f66afa6")
-        ),
-        responses(
-            (status = 204, description = "Context Map successfully deleted"),
-            (status = 400, description = "Context Map couldn't be deleted"),
-        ),
-    )]
-#[delete("/{codebase_uuid}")]
+    delete,
+    path = "/context-maps/{codebase_uuid}/{commit_hash}",
+    params(
+        ("codebase_uuid", Path, description = "Codebase UUID of the context map to delete", example = "3fa85f64-5717-4562-b3fc-2c963f66afa6"),
+        ("commit_hash", Path, description = "Git commit hash of the context map to delete", example = "abc123def4567890abc123def4567890abc123de")
+    ),
+    responses(
+        (status = 204, description = "Context Map successfully deleted"),
+        (status = 400, description = "Context Map couldn't be deleted"),
+    ),
+)]
+#[delete("/{codebase_uuid}/{commit_hash}")]
 pub async fn delete_context_map(
     service: web::Data<Arc<ContextMapServiceImpl>>,
-    codebase_uuid_path: web::Path<Uuid>,
+    path: web::Path<ProcessFilesIdentifier>,
 ) -> Result<impl Responder, ApiError> {
-    let codebase_uuid = codebase_uuid_path.into_inner();
-    service.delete(codebase_uuid).await?;
+    let identifier = path.into_inner();
+
+    service
+        .delete(identifier.codebase_uuid, &identifier.commit_hash)
+        .await?;
 
     Ok(HttpResponse::NoContent())
 }
--- synthesizer/src/contextmap/queries.rs
+++ synthesizer/src/contextmap/queries.rs
@@ -1,32 +1,32 @@
 pub const CREATE_CONTEXT_MAP: &str = r#"
-MATCH (e:Entity {codebase_uuid: $codebase_uuid})
-DETACH DELETE e;
+CALL {
+    MATCH (e:Entity {codebase_uuid: $codebase_uuid, commit_hash: $commit_hash})
+    DETACH DELETE e
+}
 
 UNWIND $entities AS entity
-MERGE (e:Entity {id: entity.signature, codebase_uuid: $codebase_uuid})
-SET e.id = entity.signature
+MERGE (e:Entity {id: entity.signature, codebase_uuid: $codebase_uuid, commit_hash: $commit_hash})
 SET e.name = entity.name
 SET e.superclasses = entity.superclasses
 SET e.fields = entity.fields
 SET e.service_name = entity.service_name
-SET e.codebase_uuid = $codebase_uuid
 
 WITH $dependencies AS dependencies
 UNWIND dependencies AS dep
-MATCH (src:Entity {id: dep.source_id, codebase_uuid: $codebase_uuid})
-MATCH (tgt:Entity {id: dep.target_id, codebase_uuid: $codebase_uuid})
-MERGE (src)-[r:DEPENDS_ON {codebase_uuid: $codebase_uuid}]->(tgt)
+MATCH (src:Entity {id: dep.source_id, codebase_uuid: $codebase_uuid, commit_hash: $commit_hash})
+MATCH (tgt:Entity {id: dep.target_id, codebase_uuid: $codebase_uuid, commit_hash: $commit_hash})
+MERGE (src)-[r:DEPENDS_ON {codebase_uuid: $codebase_uuid, commit_hash: $commit_hash}]->(tgt)
 "#;
 
 pub const GET_CONTEXT_MAP: &str = r#"
-MATCH (e:Entity {codebase_uuid: $codebase_uuid})
+MATCH (e:Entity {codebase_uuid: $codebase_uuid, commit_hash: $commit_hash})
 OPTIONAL MATCH (e)-[r:DEPENDS_ON]->(target:Entity)
-WHERE target.codebase_uuid = $codebase_uuid
+WHERE target.codebase_uuid = $codebase_uuid AND target.commit_hash = $commit_hash
 WITH collect(DISTINCT e) AS all_entities, collect(DISTINCT {source: e.id, target: target.id}) AS all_dependencies
 RETURN all_entities, all_dependencies
 "#;
 
 pub const DELETE_CONTEXT_MAP: &str = r#"
-MATCH (e:Entity {codebase_uuid: $codebase_uuid})
+MATCH (e:Entity {codebase_uuid: $codebase_uuid, commit_hash: $commit_hash})
 DETACH DELETE e
 "#;
